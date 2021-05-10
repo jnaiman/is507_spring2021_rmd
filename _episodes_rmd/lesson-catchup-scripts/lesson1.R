d0802fd03aa6a271ccd7c966edbbe9dea58c8b87
Jill Naiman <jnaiman@gmail.com>
2021-05-10 18:48:58 -0500

    moving around

--- _episodes_rmd/lesson-catchup-scripts/lesson1.R
+++
@@ -1,9 +0,0 @@
-# Lesson 1 catch-up file - current as of March 4, 2020
-
-# The first lesson is currently an introduction to the
-# workshop series and learning environment. However,
-# in case we need to add script components to it in the
-# future, we're creating the file and automating its download
-# for ease of future use.
-#
-# You can close this file and continue with lesson 2's file.
--- _episodes_rmd/trial_boundaries_jpn.R
+++
@@ -1,3 +0,0 @@
-source('./functions.R')
-
-boundary <- read_sf("/Users/jillnaiman/Downloads/agro_transformed/boundary_transformed.gpkg")
